7c3311e275c4b9fd2c4f66c8877339c206202e95
giorgi18806 <giorgi18806@gmail.com>
2025-03-10 13:11:44 +0000

    Initial commit. Add layouts

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.clean')
+
+@section('childContent')
+    @include('layouts.partials.header')
+
+    @yield('content')
+
+    <footer>
+        @section('footerLinks')
+            <a href="#">Link 1</a>
+            <a href="#">Link 2</a>
+        @show
+    </footer>
+
+@endsection 
